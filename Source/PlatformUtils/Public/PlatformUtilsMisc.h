9eb64b3a18bd1c91738d6da058c6691621a77719
hxhb <hxhb@live.com>
2020-05-28 09:17:02 +0800

    support less 4.22

--- Source/PlatformUtils/Public/PlatformUtilsMisc.h
+++ Source/PlatformUtils/Public/PlatformUtilsMisc.h
@@ -2,4 +2,16 @@
 
 #include "HAL/Platform.h"
 
-#include COMPILED_PLATFORM_HEADER(PlatformUtils.h)
+#if ENGINE_MINOR_VERSION > 22
+	#include COMPILED_PLATFORM_HEADER(PlatformUtils.h)
+#else
+	#if PLATFORM_ANDROID
+	#include "Android/AndroidPlatformUtils.h"	
+	#elif PLATFORM_IOS	
+	#include "IOS/IOSPlatformUtils.h"	
+	#elif PLATFORM_WINDOWS	
+	#include "Windows/WindowsPlatformUtils.h"	
+	#elif PLATFORM_MAC	
+	#include "Mac/MacPlatformUtils.h"	
+	#endif 
+#endif
